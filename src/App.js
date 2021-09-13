1801e7171fb416d418c0fae01ee99d48ed72a94b
bautiabella <abautista@udesa.edu.ar>
2021-09-13 10:36:46 -0300

    correcion de App.js

--- src/App.js
+++ src/App.js
@@ -11,17 +11,4 @@ function App() {
     </div>
   )}
 
-function App() {
-  return (
-    //METO LAS ETIQUETAS HERMANAS EN UNA MISMA ETIQUETA PADRE (REACT FRAGMENT aunque puede ser DIV tambien)
-    
-    <React.Fragment> 
-      <Header/> 
-       {/* EMPEZAMOS A CREAR COMPONENTES NOSOTROS */}
-      <h1> y somos el grupo 4 </h1>
-    </React.Fragment>
-    
-  );
-}
-
 export default App;
